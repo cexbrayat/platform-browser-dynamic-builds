3a38c4af0be59e6bcbc24e6517db001639ac8764
George Kalpakas <kalpakas.g@gmail.com>
2017-11-04 21:25:43 -0400

    21bfaf226 feat(elements): implement `registerAsCustomElements()`

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-bb807501c
+ * @license Angular v5.1.0-beta.0-21bfaf226
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-bb807501c');
+const VERSION = new Version('5.1.0-beta.0-21bfaf226');
 
 /**
  * @fileoverview added by tsickle
